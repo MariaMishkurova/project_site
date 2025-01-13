b29581f58f322df3bd059b56f4c7607fcf7eee33
MishkurovaMaria <marmishku@gmail.com>
2025-01-13 14:22:33 +0300

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,11 +81,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject1',
+        'USER': 'mariamiskurova',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
+
     }
 }
-
 
 
 MY_GLOBAL_VARIABLE = "Undefined"
